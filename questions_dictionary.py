472a122199b1c66011c4f9e2759183a10021da74
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-29 14:32:02 -0700

    Update file organization again

--- questions_dictionary.py
+++ questions_dictionary.py
@@ -5,7 +5,8 @@ questions_level_1 = {"Shantaram (by Australian escaped criminal Gregory David Ro
                      "What is the answer to the meaning of life according to Hitchhikers Guide to the Galaxy? "
                      "\n 1: 100\n 2: 420\n 3: 42 \n 4: 1 \n 5: Infinity": 3,
                      "Which of the follwing is not a book by Malcolm Gladwell:"
-                     "\n 1: Outwitting the Devil \n: The Tipping Point \n: Outliers \n: Talking to Strangers \n: Blink": 1,
+                     "\n 1: Outwitting the Devil \n: The Tipping Point \n: Outliers \n: Talking to Strangers \n: "
+                     "Blink": 1,
                      "Which book is the shortest? "
                      "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n 4: Clockwork Orange \n 5: Catch 22": 1,
                      "The TV show 'Big Brother' is based on the all-seeing entity from which book:"
@@ -22,30 +23,37 @@ questions_level_2 = {"What are the main characters in Never Let Me Go? "
                      "\n 1: Asia\n 2: Australia\n 3: South America \n 4: North America \n 5: Africa": 5,
                      "When the Body Says no (by Canadian doctor Gabor Mate) main message is: "
                      "\n 1: Always listen to your gut\n 2: If you don't rest you will get tired\n 3: If you don't "
-                     "look after your mental health, your physical health will suffer \n 4: Drugs are bad \n 5: XXXXX": 2,
+                     "look after your mental health, your physical health will suffer \n 4: Drugs are bad \n "
+                     "5: XXXXX": 2,
                      "Marching Powder is set in:"
                      "\n 1: Colombia \n 2: Bolivia\n 3: Mexico \n 4: Peru \n 5: Panama": 2,
                      "What is a supposedly fun thing that David Foster Wallace will never do again?"
-                     "\n 1: Go on a cruise Ship \n 2: Oktoberfest in Germany \n 3: Run with the bulls \n 4: Carnival in Brazil "
+                     "\n 1: Go on a cruise Ship \n 2: Oktoberfest in Germany \n 3: Run with the bulls \n 4: Carnival "
+                     "in Brazil "
                      "\n 5: New Years ball drop in New York ": 1,
                      "All the light we cannot see is set in:"
                      "\n 1: Space \n 2: WWI \n 3: The future \n 4: Ancient Greece \n 5: In dream": 2,
                      "Pain is inevitable, suffering is optional was made famous by:"
-                     "\n 1: George Orwell \n 2: Haruki Murakami \n 3: Alice Walker \n 4: Malcolm Gladwell \n 5: Dale Carnegie": 2,
+                     "\n 1: George Orwell \n 2: Haruki Murakami \n 3: Alice Walker \n 4: Malcolm Gladwell \n 5: Dale"
+                     " Carnegie": 2,
                      "'First, make the beast beautiful' is a book about:"
                      "\n 1: Beauty pageants \n 2: Anxiety  \n 3: Children \n 4: Overcoming loss \n 5: High school": 2
                      }
 
 questions_level_3 = {"What is NOT presented as true in Breathe by James Nestor"
-                     "\n 1: Mouth breathing is awful for you\n 2: Modern day diet negatively effects our facial structure\n "
-                     "3: Having out-breath SHORTER than in-breath lowers anxiety \n 4: Importance of breathe is highly underrated"
+                     "\n 1: Mouth breathing is awful for you\n 2: Modern day diet negatively effects our facial "
+                     "structure\n "
+                     "3: Having out-breath SHORTER than in-breath lowers anxiety \n 4: Importance of breathe is highly "
+                     "underrated"
                      "  \n 5: Most people are over-breathers (too fast and too shallow": 3,
-                     "What movie actually complements the book really well (subjective but also definitely a correct answer :P) "
+                     "What movie actually complements the book really well (subjective but also definitely a correct "
+                     "answer :P) "
                      "\n 1: Cloud Atlas\n 2: Tomorrow When the War Began\n 3: ??? \n 4: ??? \n 5: ???": 1,
                      "Which Haruki Murakmi novel has no fantasy elements"
                      "\n 1: Hard Boiled Wonderland and the Edge of The World\n 2: Kafka on the Shore"
                      "\n 3: Wild Sheep Chase \n 4: Wind up Bird Chronicle \n 5: Norwegian Wood": 5,
-                     "What is the phenomenon called where 'those places ... where all the different kinds of truths fit together \n"
+                     "What is the phenomenon called where 'those places ... where all the different kinds of truths fit"
+                     " together \n"
                      "in Sirens of Titan "
                      "\n 1: ???\n 2: ???\n 3: ??? \n 4: Tralfamadorian \n 5: chrono-synclastic infundibulum": 5,
                      "What is the message of The Pearl (John Steinbeck) "
@@ -55,7 +63,8 @@ questions_level_3 = {"What is NOT presented as true in Breathe by James Nestor"
                      "\n 1: Yes \n 2: No": 2,
                      "What is not one of the 12 rules of life, according to Jordan Peterson:"                    "18"
                      "\n 1: Make friends with people who want the best for you \n 2: Compare yourself to who you were "
-                     "yesterday, not to who someone else is today. \n 3: Stand up straight with your shoulders back. \n 4: "
+                     "yesterday, not to who someone else is today. \n 3: Stand up straight with your shoulders back. \n"
+                     " 4: "
                      "Opportunities don't happen, you create them \n 5: Tell the truth--or, at least, don't lie": 4,
                      "In 'Wind up Bird Chronicle', the place the protagonist goes into a different world from is:"
                      "\n 1: The ocean \n 2: Under a waterfall  \n 3: A well \n 4: In the basement of his house "
