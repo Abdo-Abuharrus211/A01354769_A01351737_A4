90c7da33925ee4db4fa837e728a90212869ab29a
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-01 09:17:56 -0700

    Update unit tests

--- test_game_state_control_check_for_foes.py
+++ test_game_state_control_check_for_foes.py
@@ -1,6 +1,20 @@
+
 from unittest import TestCase
+from unittest.mock import patch
+
+from game_state_control import check_for_foes
 
 
-class Test(TestCase):
-    def test_check_for_foes(self):
-        self.fail()
+class TestTestForFoes(TestCase):
+
+    @patch('random.randint', return_value=2)
+    def test_check_for_foes_none(self, _):
+        check_for_foes()
+        expected_output = True
+        self.assertEqual(expected_output, check_for_foes())
+
+    @patch('random.randint', return_value=4)
+    def test_check_for_foes_present(self, _):
+        check_for_foes()
+        expected_output = False
+        self.assertEqual(expected_output, check_for_foes())
