66ff22ad3097478c1c26af88fc7cf9ee5aa4b0c0
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-01 08:31:45 -0700

    Update unit tests

--- test_game_mechanics_move_character.py
+++ test_game_mechanics_move_character.py
@@ -4,33 +4,33 @@ from unittest import TestCase
 
 class TestMoveCharacter(TestCase):
 
-    def test_move_character_1(self):
-        character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-        move_character(character_location, "1")
-        self.assertEqual({'X-coordinate': 1, 'Y-coordinate': 0, 'Current HP': 5}, character_location)
-
-    def test_move_character_2(self):
-        character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-        move_character(character_location, "2")
-        self.assertEqual({'X-coordinate': 1, 'Y-coordinate': 2, 'Current HP': 5}, character_location)
-
-    def test_move_character_3(self):
-        character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-        move_character(character_location, "3")
+    def test_move_character_S(self):
+        character_location = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+        move_character(character_location, "S")
         self.assertEqual({'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 5}, character_location)
 
-    def test_move_character_not_allowed(self):
+    def test_move_character_N(self):
         character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-        move_character(character_location, "6")
-        self.assertEqual({'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}, character_location)
+        move_character(character_location, "N")
+        self.assertEqual({'X-coordinate': 1, 'Y-coordinate': 0, 'Current HP': 5}, character_location)
 
-    def test_move_character_4(self):
-        character_location = {"X-coordinate": 1, "Y-coordinate": 2, "Current HP": 5}
-        move_character(character_location, "4")
-        self.assertEqual({'X-coordinate': 2, 'Y-coordinate': 2, 'Current HP': 5}, character_location)
+    def test_move_character_E(self):
+        character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
+        move_character(character_location, "E")
+        self.assertEqual({'X-coordinate': 2, 'Y-coordinate': 1, 'Current HP': 5}, character_location)
+
+    def test_move_character_W(self):
+        character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
+        move_character(character_location, "W")
+        self.assertEqual({'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 5}, character_location)
 
     def test_move_character_not_allowed_letter(self):
+        character_location = {"X-coordinate": 0, "Y-coordinate": 2, "Current HP": 5}
+        move_character(character_location, "T")
+        self.assertEqual({'X-coordinate': 0, 'Y-coordinate': 2, 'Current HP': 5}, character_location)
+
+    def test_move_character_not_allowed_number(self):
         character_location = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-        move_character(character_location, "h")
+        move_character(character_location, "6")
         self.assertEqual({'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}, character_location)
 
