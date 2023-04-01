ad9dd86783d00227636fa045bb42fe11f86f5c71
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-01 09:39:31 -0700

    Update unit test

--- test_game_state_control_level_up.py
+++ test_game_state_control_level_up.py
@@ -1,6 +1,9 @@
+from game_state_control import level_up
 from unittest import TestCase
 
 
-class Test(TestCase):
-    def test_level_up(self):
-        self.fail()
+class TestLevelUp(TestCase):
+
+    def test_level_up_yes(self):
+        character_test = {"Current XP": 360, "Knowledge": 1}
+        self.assertEqual(True, dead_yet(character_test))
