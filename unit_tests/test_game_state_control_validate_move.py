3d05d85e876c072dc6e19cd5b3ee956ffbe9750c
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-01 20:03:09 -0700

    Add unit tests

--- unit_tests/test_game_state_control_validate_move.py
+++ unit_tests/test_game_state_control_validate_move.py
@@ -1,6 +1,52 @@
 from unittest import TestCase
 
+from game_state_control import validate_move
 
-class Test(TestCase):
-    def test_validate_move(self):
-        self.fail()
+
+class TestValidateMove(TestCase):
+    def test_validate_move_south(self):
+        self.assertEqual(True, validate_move({(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"},
+        {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}, "S"))
+
+    def test_validate_move_north(self):
+        self.assertEqual(True, validate_move({(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"},
+        {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 5}, "N"))
+
+    def test_validate_move_east(self):
+        self.assertEqual(True, validate_move({(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"},
+        {"X-coordinate": 0, "Y-coordinate": 1, "Current HP": 5}, "E"))
+
+    def test_validate_move_west(self):
+        self.assertEqual(True, validate_move({(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"},
+        {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 5}, "W"))
+
+    def test_validate_move_key_error_E(self):
+        with self.assertRaises(KeyError):
+            test_board = {(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"}
+            test_character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 5}
+            test_direction = "E"
+            validate_move(test_board, test_character, test_direction)
+
+    def test_validate_move_key_error_W(self):
+        with self.assertRaises(KeyError):
+            test_board = {(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"}
+            test_character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+            test_direction = "W"
+            validate_move(test_board, test_character, test_direction)
+
+    def test_validate_move_key_error_N(self):
+        with self.assertRaises(KeyError):
+            test_board = {(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"}
+            test_character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+            test_direction = "N"
+            validate_move(test_board, test_character, test_direction)
+
+    def test_validate_move_key_error_S(self):
+        with self.assertRaises(KeyError):
+            test_board = {(0, 0): "Potato", (0, 1): "Pie", (1, 0): "Cheese", (1, 1): "Burger"}
+            test_character = {"X-coordinate": 0, "Y-coordinate": 1, "Current HP": 5}
+            test_direction = "S"
+            validate_move(test_board, test_character, test_direction)
+
+
+
