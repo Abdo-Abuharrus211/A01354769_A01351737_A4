794f443235b08916015d7f175cbc950cf3ef724f
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-02 18:06:52 -0700

    Update guessing game uni tests

--- unit_tests/test_guessing_game.py
+++ unit_tests/test_guessing_game.py
@@ -8,14 +8,11 @@ from user_io import guessing_game
 class TestGuessingGame(TestCase):
 
     @patch('builtins.input', side_effect=[2])
-    @patch('random.choice', return_value="What are the main characters in Never Let Me Go? "
-                                         "\n 1: Clones\n 2: Humans\n 3: Robots \n 4: Faceless AI \n 5: Dream characters")
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_guessing_game_correct_print(self, mock_output, _, __):
+    def test_guessing_game_correct_print(self, mock_output, _):
         character = {"Current HP": 4, "Knowledge": 2}
         guessing_game(character)
-        game_prints_this = mock_output.getvalue()
-        expected_output = "Incorrect, 1 hit taken\n"
+        test_real_answer = 2
+        expected_output = "You may pass unharmed\n"
         self.assertEqual(expected_output, game_prints_this)
 
     @patch('builtins.input', side_effect=[3])
