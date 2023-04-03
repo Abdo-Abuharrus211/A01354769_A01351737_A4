4a2f12b8867a3e44621d2960fe167e3200bc4ec2
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-03 14:13:03 -0700

    Add level_up() unit tests

--- unit_tests/test_level_up.py
+++ unit_tests/test_level_up.py
@@ -4,8 +4,23 @@ from unittest import TestCase
 
 class TestLevelUp(TestCase):
 
-    def test_level_up_yes(self):
-        character_test = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Current XP": 120, "Knowledge": 1}
+    def test_level_up_no(self):
+        character_test = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Current XP": 70,
+                          "Knowledge": "Novice"}
+        level_up(character_test)
+        expected_level_up = "Novice"
+        self.assertEqual(expected_level_up, character_test["Knowledge"])
+
+    def test_level_up_yes_bookworm(self):
+        character_test = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Current XP": 124,
+                          "Knowledge": "Novice"}
         level_up(character_test)
         expected_level_up = "Bookworm"
+        self.assertEqual(expected_level_up, character_test["Knowledge"])
+
+    def test_level_up_yes_master_custodian(self):
+        character_test = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Current XP": 205,
+                          "Knowledge": "Bookworm"}
+        level_up(character_test)
+        expected_level_up = "Master Custodian"
         self.assertEqual(expected_level_up, character_test["Knowledge"])
