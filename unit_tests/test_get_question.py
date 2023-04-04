03e86dcf0581eec16eb950fead9c25a0d6158ba5
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-04-03 19:43:39 -0700

    Update get_questions.. to delete

--- unit_tests/test_get_question.py
+++ unit_tests/test_get_question.py
@@ -1,9 +1,31 @@
 from unittest import TestCase
+import io
+from unittest.mock import patch
 
-from user_io import get_question
+
+from guessing_game import get_question
 
 
 class Test(TestCase):
-    def test_get_question_novice_level(self):
-        char_one = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "Current XP": 20,
-                    "Knowledge": "Novice"}
+    @patch('random.choice', return_value="What is NOT presented as true in Breathe by James Nestor")
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_check_for_foes_none(self, mock_output, _):
+        test_character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "Current XP": 20,
+                        "Knowledge": "Novice"}
+        get_question(test_character)
+        the_game_printed_this = mock_output.getvalue()
+        expected_output = "Test output"
+        self.assertEqual(expected_output, the_game_printed_this)
+
+#     @patch('sys.stdout', new_callable=io.StringIO)
+#     def test_simple_game(self, mock_output, random_number_generator, mock_input):
+#         simple_game()
+#         the_game_printed_this = mock_output.getvalue()
+#         expected_output = "You're right!\n"
+#         self.assertEqual(expected_output, the_game_printed_this)
+#
+#
+# class Test(TestCase):
+#     def test_get_question_novice_level(self):
+#         char_one = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "Current XP": 20,
+#                     "Knowledge": "Novice"}
