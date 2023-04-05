6a45c866f6bbf26ffccca038eead6055f36460ea
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-05 09:23:57 -0700

    Add exception raise check_for_final_boss & unit test

--- game_state_control.py
+++ game_state_control.py
@@ -35,6 +35,7 @@ def validate_move(board: dict, character: dict, direction: str) -> bool:
      >>> validate_move(board_three, character_three, direction_three)
      True
      """
+
     if direction == "N" and 0 <= character["Y-coordinate"] - 1:
         valid_move = True
     elif direction == "S" and character["Y-coordinate"] + 1 <= max(board)[1]:
@@ -79,6 +80,11 @@ def dead_yet(character: dict) -> dict:
      >>> dead_yet(character_three)
      True
     """
+    stat_list = ['X-coordinate', 'Y-coordinate', 'Current HP', 'Current XP', 'Knowledge']
+    for attribute in stat_list:
+        if attribute not in list(character.keys()):
+            raise KeyError("Character Attribute not found")
+
     return character["Current HP"] == 0
 
 
@@ -96,6 +102,11 @@ def damage_received(character):
     >>> damage_received(chad)
     7
     """
+    stat_list = ['X-coordinate', 'Y-coordinate', 'Current HP', 'Current XP', 'Knowledge']
+    for attribute in stat_list:
+        if attribute not in list(character.keys()):
+            raise KeyError("Character Attribute not found")
+
     if character["Knowledge"] == "Master Custodian":
         damage_inflicted = 3
     elif character["Knowledge"] == "Bookworm":
@@ -151,6 +162,11 @@ def check_for_final_boss(character: dict) -> bool:
     :postcondition: generates random integer in a specific range then check if it's equal or greater than 25%
     :return: True if there's a 20% or more chance of facing a foe, return False otherwise
     """
+    stat_list = ['X-coordinate', 'Y-coordinate', 'Current HP', 'Current XP', 'Knowledge']
+    for attribute in stat_list:
+        if attribute not in list(character.keys()):
+            raise KeyError("Character Attribute not found")
+
     random_chance = random.randint(1, 10) >= 4
     if character["Current XP"] >= 300 and random_chance:
         return True
