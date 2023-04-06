7996c4583c956b1deda357b7e729e248d27014b4
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-06 11:05:47 -0700

    Update game_mechanics.py docstrings

--- game_mechanics.py
+++ game_mechanics.py
@@ -8,7 +8,8 @@ def describe_current_location(board: dict, character: dict):
     Describe the room the player is currently in.
 
     :param board: dictionary representing the game board's rooms, with (x,y) coordinates as keys and string values.
-    :param character: dictionary representing player's character stats, current location and HP
+    :param character: dictionary of representing the character and their attributes including X-Coordinate, Y-Coordinate
+            Current HP, Current XP, &  Knowledge
     :precondition: board and character are dictionaries and not None types
     :postcondition: obtains player's current location to acquire room's description from board and prints it
 
@@ -42,7 +43,8 @@ def move_character(character: dict, direction: str):
     """
     Update character's current location on the board.
 
-    :param character: dictionary representing player's character stats, current location and HP
+    :param character: ictionary of representing the character and their attributes including X-Coordinate, Y-Coordinate
+            Current HP, Current XP, &  Knowledge
     :param direction: a string for the direction to move the player towards
     :precondition: character is valid dictionary with character stats, and direction is a string of 'N','E','S', or 'W'
     :postcondition: updates character's coordinates correctly according to direction the player moves in
@@ -51,6 +53,11 @@ def move_character(character: dict, direction: str):
      >>> direction_one = "S"
      >>> move_character(character_one, direction_one)
      >>> print(character_one["Y-coordinate"])
+     1
+     >>> character_two = {"X-coordinate": 2, "Y-coordinate": 2, "Current HP": 50, "Current XP": 50, "Knowledge": "bob"}
+     >>> direction_two ="W"
+     >>> move_character(character_two, direction_two)
+     >>> print(character_two["X-coordinate"])
      1
     """
     stat_list = ['X-coordinate', 'Y-coordinate']
