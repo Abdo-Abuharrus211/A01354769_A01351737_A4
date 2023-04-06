04059453ac1e46aefab849b238228af5888acdf6
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-06 13:21:41 -0700

    modify make_character() unit test

--- test_make_character.py
+++ test_make_character.py
@@ -5,5 +5,5 @@ from assets import make_character
 
 class TestMakeCharacter(TestCase):
     def test_make_character(self):
-        expectd_char = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
-        self.assertEqual(expectd_char, make_character())
+        expected_char = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
+        self.assertEqual(expected_char, make_character())
