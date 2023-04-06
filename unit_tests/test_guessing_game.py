5d9e1f24ecd8a40b761d0effdd386c93cfb0660e
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-06 15:07:29 -0700

    Fix all guessing_game() unit tests

--- unit_tests/test_guessing_game.py
+++ unit_tests/test_guessing_game.py
@@ -7,84 +7,138 @@ from guessing_game import guessing_game
 
 class TestGuessingGame(TestCase):
 
-    @patch('builtins.input', side_effect=["T"])
-    def test_guessing_game_value_error_letter(self, _):
-        with self.assertRaises(ValueError):
-            character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
-            guessing_game(character)
-
-        @patch('builtins.input', side_effect=[2])
-        @patch('guessing.game.get_question')
-        @patch('sys.stdout', new_callable=io.StringIO)
-        def test_guessing_game_correct_print(self, mock_output, _, __):
-            character = {"Current HP": 4, "Knowledge": 2}
-
-            guessing_game(character)
-            expected_output = "You may pass unharmed\n"
-            self.assertIn(expected_output, mock_output.getvalue())
-
-    @patch('builtins.input', side_effect=[3])
     @patch('random.randint', return_value=2)
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_guessing_game_incorrect_print(self, mock_output, _, __):
-        character = {"Current HP": 5}
-        guessing_game(character)
-        game_prints_this = mock_output.getvalue()
-        expected_output = "Incorrect, 1 hit taken\n"
-        self.assertEqual(expected_output, game_prints_this)
-
-    @patch('builtins.input', side_effect=[9])
-    @patch('random.randint', return_value=2)
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_guessing_game_out_of_range_print(self, mock_output, _, __):
-        character = {"Current HP": 5}
-        guessing_game(character)
-        game_prints_this = mock_output.getvalue()
-        expected_output = "Please pick a number between 1 and 5 inclusive, you lost 1 HP\n"
-        self.assertEqual(expected_output, game_prints_this)
-
-    @patch('builtins.input', side_effect=["kate"])
-    @patch('random.randint', return_value=2)
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_guessing_game_out_non_number_print(self, mock_output, _, __):
-        character = {"Current HP": 5}
-        guessing_game(character)
-        game_prints_this = mock_output.getvalue()
-        expected_output = "Please pick a number between 1 and 5 inclusive, you lost 1 HP\n"
-        self.assertEqual(expected_output, game_prints_this)
-
-    @patch('builtins.input', side_effect=[2])
-    @patch('random.randint', return_value=2)
-    def test_guessing_game_correct_print_health(self, _, __):
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=[1])
+    def test_guessing_game_correct_print_health(self, _, __, ___):
         character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 30, "Current XP": 0, "Knowledge": "Novice"}
         guessing_game(character)
-        expected_output = {"Current HP": 5}
+        expected_output = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 30, "Current XP": 20,
+                           "Knowledge": "Novice"}
         actual_output = character
         self.assertEqual(expected_output, actual_output)
 
-    @patch('builtins.input', side_effect=[2])
-    @patch('random.randint', return_value=3)
-    def test_guessing_game_incorrect_print_health(self, _, __):
-        character = {"Current HP": 5}
+    # @patch('builtins.input', side_effect=["T"])
+    # def test_guessing_game_value_error_letter(self, _):
+    #     with self.assertRaises(ValueError):
+    #         character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
+    #         guessing_game(character)
+    #
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=[1])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_guessing_game_correct_print(self, mock_output, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
         guessing_game(character)
-        expected_output = {"Current HP": 4}
-        actual_output = character
+        actual_output = mock_output.getvalue()
+        expected_output = "An Orb Weaver has blocked the path with its sticky web. " \
+                          "You must answer the question to persevere! \n" \
+                          " Which book is the shortest? \n " \
+                          "1: Animal Farm \n " \
+                          "2: 1984\n " \
+                          "3: Lord of the Flies \n " \
+                          "4: Clockwork Orange \n " \
+                          "5: Catch 22\n" \
+                          "You may pass unharmed\n"
         self.assertEqual(expected_output, actual_output)
 
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=[5])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_guessing_game_incorrect_print(self, mock_output, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
+        guessing_game(character)
+        game_prints_this = mock_output.getvalue()
+        expected_output = "An Orb Weaver has blocked the path with its sticky web. " \
+                          "You must answer the question to persevere! \n" \
+                          " Which book is the shortest? \n " \
+                          "1: Animal Farm \n " \
+                          "2: 1984\n " \
+                          "3: Lord of the Flies \n " \
+                          "4: Clockwork Orange \n " \
+                          "5: Catch 22\n" \
+                          "'Incorrect, 1 hit taken'\n"
+        self.assertEqual(expected_output, game_prints_this)
+
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=[7])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_guessing_game_out_of_range_print(self, mock_output, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 30, "Current XP": 0, "Knowledge": "Novice"}
+        guessing_game(character)
+        game_prints_this = mock_output.getvalue()
+        expected_output = "An Orb Weaver has blocked the path with its sticky web. " \
+                          "You must answer the question to persevere! \n" \
+                          " Which book is the shortest? \n " \
+                          "1: Animal Farm \n " \
+                          "2: 1984\n " \
+                          "3: Lord of the Flies \n " \
+                          "4: Clockwork Orange \n " \
+                          "5: Catch 22\n" \
+                          "'You must pick a number between 1 and 5 inclusive, you lost 1 HP'\n"
+        self.assertEqual(expected_output, game_prints_this)
+
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
     @patch('builtins.input', side_effect=["kate"])
-    @patch('random.randint', return_value=3)
-    def test_guessing_game_non_number_health(self, _, __):
-        character = {"Current HP": 5}
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_guessing_game_out_non_number_print(self, mock_output, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
         guessing_game(character)
-        expected_output = {"Current HP": 4}
+        game_prints_this = mock_output.getvalue()
+        expected_output = "An Orb Weaver has blocked the path with its sticky web. " \
+                          "You must answer the question to persevere! \n" \
+                          " Which book is the shortest? \n " \
+                          "1: Animal Farm \n " \
+                          "2: 1984\n " \
+                          "3: Lord of the Flies \n " \
+                          "4: Clockwork Orange \n " \
+                          "5: Catch 22\n" \
+                          "'Please pick a number between 1 and 5 inclusive, you lost 1 HP'\n"
+        self.assertEqual(expected_output, game_prints_this)
+
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=["kate"])
+    def test_guessing_game_incorrect_print_health(self, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 30, "Current XP": 0, "Knowledge": "Novice"}
+        guessing_game(character)
         actual_output = character
+        expected_output = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 20, "Current XP": 0,
+                           "Knowledge": "Novice"}
         self.assertEqual(expected_output, actual_output)
 
-    @patch('builtins.input', side_effect=[9])
-    @patch('random.randint', return_value=3)
-    def test_guessing_game_non_number_health(self, _, __):
-        character = {"Current HP": 3}
+    @patch('random.randint', return_value=2)
+    @patch('random.choice', side_effect={"Which book is the shortest? "
+                                         "\n 1: Animal Farm \n 2: 1984\n 3: Lord of the Flies \n"
+                                         " 4: Clockwork Orange \n 5: "
+                                         "Catch 22": 1})
+    @patch('builtins.input', side_effect=["kate"])
+    def test_guessing_game_non_number_health(self, _, __, ___):
+        character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 50, "Current XP": 0, "Knowledge": "Novice"}
         guessing_game(character)
-        expected_output = {"Current HP": 2}
+        expected_output = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 40, "Current XP": 0,
+                           "Knowledge": "Novice"}
         actual_output = character
         self.assertEqual(expected_output, actual_output)
